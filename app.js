9e93b56a82b436eac11aa1c170e713f51392456f
Dolue <arinzechukwudolue@gmail.com>
2023-02-14 02:23:27 +0100

    password removed

--- app.js
+++ app.js
@@ -11,4 +11,4 @@ app.use("/api/blog", blogRouter)
 
 mongoose.set("strictQuery", false);
 
-mongoose.connect("mongodb+srv://arinze:Spade123456789@clusterar.pc1xkig.mongodb.net/social-media?retryWrites=true&w=majority").then(() => app.listen(5000)).then(() => console.log("Connected Bitches!!")).catch((err) => console.log(err));
+mongoose.connect("mongodb+srv://++++++:++++++++++@clusterar.pc1xkig.mongodb.net/social-media?retryWrites=true&w=majority").then(() => app.listen(5000)).then(() => console.log("Connected Bitches!!")).catch((err) => console.log(err));
